c4a8130140967abd7af61d03419d6aaada9f506c
a.zubkov <zubkoffalex4nder@yandex.ru>
2025-02-06 16:32:29 +0300

    init

---
+++ app/src/pages/IndexPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    
+  </q-page>
+</template>
+
+<script>
+
+  import { defineComponent } from "vue";
+  import axios from "axios";
+
+
+  export default defineComponent ({
+
+    name: 'PageIndex',
+
+    setup () {
+
+      axios.get('http://localhost/sanctum/csrf-cookie')
+
+        .then(response => {
+
+          console.log(response)
+          
+        })
+
+    }
+  })
+</script>
